9748fb12c5bc09fd3e3f954942c69b6f7cdfb7a4
GwenL3013 <gwenck2@gmail.com>
2025-03-28 16:13:58 +0800

    added firestore local storage

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,51 +1,63 @@
-import { Col, Image, Modal, Row, Form, Button } from "react-bootstrap";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import useLocalStorage from "use-local-storage";
+import {
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+} from "firebase/auth";
+import { useEffect, useContext, useState } from "react";
+import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap";
+import { AuthContext } from "../components/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
 export default function AuthPage() {
     const loginImage = "https://sig1.co/img-twitter-1";
-    const url =
-        "https://814163f1-5915-4866-a25a-63bc5af4cc80-00-22agp2rgjjcns.sisko.replit.dev";
-    const [modalShow, setModalShow] = useState(false);
-    const handleShowSignUp = () => setModalShow("SignUp");
-    const handleShowLogin = () => setModalShow("Login");
+    // values: null (no modal show), "login", "signup"
+    const [modalShow, setModalShow] = useState(null);
+    const handleShowSignUp = () => setModalShow("signup");
+    const handleShowLogin = () => setModalShow("login");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [authToken, setAuthToken] = useLocalStorage("authToken", "");
-
+    const auth = getAuth();
     const navigate = useNavigate();
+    const { currentUser } = useContext(AuthContext);
 
     useEffect(() => {
-        if (authToken) {
-            navigate("/profile");
-        }
-    }, [authToken, navigate])
+        if (currentUser) navigate("/profile");
+    }, [currentUser, navigate]);
 
     const handleSignUp = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`${url}/signup`, { username, password });
-            console.log(res.data);
+            const res = await createUserWithEmailAndPassword(
+                auth,
+                username,
+                password
+            );
+            console.log(res.user);
         } catch (error) {
             console.error(error);
         }
     };
-
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`${url}/login`, { username, password });
-            if (res.data && res.data.auth === true && res.data.token) {
-                setAuthToken(res.data.token);//save toekn to local storage
-                console.log("Login was successful, token saved");
-            }
-
+            await signInWithEmailAndPassword(auth, username, password);
         } catch (error) {
             console.error(error);
         }
     };
+
+    const provider = new GoogleAuthProvider();
+    const handleGoogleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithPopup(auth, provider);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     const handleClose = () => setModalShow(null);
 
     return (
@@ -54,13 +66,23 @@ export default function AuthPage() {
                 <Image src={loginImage} fluid />
             </Col>
             <Col sm={6} className="p-4">
-                <i className="bi bi-twitter" style={{ fontSize: 50, color: "dodgerblue" }}></i>
+                <i
+                    className="bi bi-twitter"
+                    style={{ fontSize: 50, color: "dodgerblue" }}
+                ></i>
 
-                <p className="mt-5" style={{ fontSize: 64 }}>Happening Now</p>
-                <h2 className="my-5" style={{ fontSize: 31 }}>Join Twitter Today</h2>
-
+                <p className="mt-5" style={{ fontSize: 64 }}>
+                    Happening Now
+                </p>
+                <h2 className="my-5" style={{ fontSize: 31 }}>
+                    Join Twitter today.
+                </h2>
                 <Col sm={5} className="d-grid gap-2">
-                    <Button className="rounded-pill" variant="outline-dark">
+                    <Button
+                        className="rounded-pill"
+                        variant="outline-dark"
+                        onClick={handleGoogleLogin}
+                    >
                         <i className="bi bi-google"></i> Sign up with Google
                     </Button>
                     <Button className="rounded-pill" variant="outline-dark">
@@ -70,11 +92,8 @@ export default function AuthPage() {
                     <Button className="rounded-pill" onClick={handleShowSignUp}>
                         Create an account
                     </Button>
-                    <p style={{ fontSize: "12px" }}>
-                        By signing up, you agree to the Terms of Service and Privacy Policy including Cookie Use
-                    </p>
-
-                    <p className="mt-5" style={{ fontWeight: 'bold' }}>
+                    <p style={{ fontSize: "12px" }}> Agree to terms</p>
+                    <p className="mt-5" style={{ fontWeight: "bold" }}>
                         Already have an account?
                     </p>
                     <Button
@@ -82,9 +101,8 @@ export default function AuthPage() {
                         variant="outline-primary"
                         onClick={handleShowLogin}
                     >
-                        Sign In
+                        Sign in
                     </Button>
-
                 </Col>
                 <Modal
                     show={modalShow !== null}
@@ -94,24 +112,24 @@ export default function AuthPage() {
                 >
                     <Modal.Body>
                         <h2 className="mb-4" style={{ fontWeight: "bold" }}>
-                            {modalShow === "SignUp"
+                            {modalShow === "signup"
                                 ? "Create your account"
                                 : "Log in to your account"}
                         </h2>
 
                         <Form
-                            className="d-grip gap-2 px-5"
-                            onSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
+                            className="d-grid gap-2 px-5"
+                            onSubmit={modalShow === "signup" ? handleSignUp : handleLogin}
                         >
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Group className="mb-3" controlId="formBasicEmai">
                                 <Form.Control
                                     onChange={(e) => setUsername(e.target.value)}
                                     type="email"
-                                    placeholder="Enter username"
+                                    placeholder="Enter email"
                                 />
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Control
                                     onChange={(e) => setPassword(e.target.value)}
                                     type="password"
@@ -119,18 +137,17 @@ export default function AuthPage() {
                                 />
                             </Form.Group>
 
-                            <p style={{ fontSize: "12px" }}>
-                                By signing up, you agree to the Terms of Service and Privacy Policy,
-                                including Cookie Use. SigmaTweets may use your contact information,
-                                including your email address and phone number for purposes outlined in our Privacy Policy,
-                                like keeping your account secure and personalising our services, including ads. Learn more.
-                                Others will be able to find you by email or phone number, when provided,
-                                unless you choose otherwise here.
+                            <p style={{ fontSize: 12 }}>
+                                By signing up, you agree to the Terms of Service and Privacy
+                                Policy, including Cookie Use. SigmaTweets may use your contact
+                                information, including your email address and phone number for
+                                purposes outlined in our Privacy Policy, like keeping your
+                                account secure and personalising our services, including ads.
+                                Learn more. Others will be able to find you by email or phone
+                                number, when provided, unless you choose otherwise here.
                             </p>
-
                             <Button className="rounded-pill" type="submit">
-                                {modalShow === "SignUp" ? "Sign up" : "Log in"}
-
+                                {modalShow === "signup" ? "Sign up" : "Log in"}
                             </Button>
                         </Form>
                     </Modal.Body>
